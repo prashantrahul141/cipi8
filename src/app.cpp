85d54c1cd10720d6c247555fbf1ef90e783797b6
Prashant <prashantrahul141@protonmail.com>
2024-09-21 16:03:53 +0530

    add: parse command line arguments using argparse

--- src/app.cpp
+++ src/app.cpp
@@ -2,16 +2,33 @@
 
 // constructor.
 App::App(int argc, char *argv[]) {
-  // no file given.
-  if (argc < 2) {
-    nhlog_error("No rom file given.");
-    exit(1);
+  // parse command line arguments.
+  argparse::ArgumentParser program("cipi8", "1.0.0");
+  program.add_argument("rom_file").help("The rom file to run.").required();
+  program.add_argument("--scale")
+      .help("Scale of the display")
+      .default_value(10)
+      .scan<'i', int>();
+
+  program.add_argument("--delay")
+      .help("Delay between CPU cycles.")
+      .default_value(1)
+      .scan<'i', int>();
+
+  try {
+    program.parse_args(argc, argv);
+  } catch (const std::exception &err) {
+    std::cerr << err.what() << std::endl;
+    std::cerr << program;
+    std::exit(1);
   }
 
-  // file doesnt exist
-  if (!std::filesystem::exists(argv[1])) {
+  auto raw_filename = program.get<std::string>("rom_file");
+
+  // if file doesnt exist
+  if (!std::filesystem::exists(raw_filename)) {
     nhlog_error("Given file does not exist.");
-    exit(1);
+    exit(EXIT_FAILURE);
   }
 
   this->filename = argv[1];
--- src/app.h
+++ src/app.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "chip8.h"
+#include "external/argparse.hpp"
 #include "external/nhlog.h"
 #include <cstdlib>
 #include <filesystem>
